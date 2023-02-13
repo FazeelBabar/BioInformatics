485fff86153854958c03d432a16056bc30d55812
FazeelBabar <125257416+FazeelBabar@users.noreply.github.com>
2023-02-13 14:18:18 +0500

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,6 @@
 import streamlit as st
+choosen_algo=st.radio(label='Choose Algorithm',options=['Needleman','Smith_Waterman'])
 seq_1=st.text_input("Enter Sequence 1")
+seq_2=st.text_input("Enter Sequence 2")
 button=st.button("Submit")
+
