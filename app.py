a1a1c7c7d86a14f07d2305c35dd91eef2bf7bdf9
FazeelBabar <125257416+FazeelBabar@users.noreply.github.com>
2023-02-13 15:47:37 +0500

    Update app.py

--- app.py
+++ app.py
@@ -120,7 +120,7 @@ def smith_waterman(seq1, seq2):
 #choosen_algo=st.radio(label='Choose Algorithm',options=['Needleman','Smith_Waterman'])
 seq_1=st.text_input("Enter Sequence 1")
 seq_2=st.text_input("Enter Sequence 2")
-if st.button("Needleman") == "Needleman":
-  needleman_wunsch(seq_1, seq_2)
-elif st.button("Waterman") == "Waterman":
-  smith_waterman(seq_1, seq_2)
+st.button("Submit")
+needleman_wunsch(seq_1, seq_2)
+#elif st.button("Waterman") == "Waterman":
+smith_waterman(seq_1, seq_2)
